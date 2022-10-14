4c093a2ee0ed88e4f04577ce0de6b8371389bfd4
nihano <nioztekin@gmail.com>
2022-10-14 15:01:37 +0300

    number of people in the bus

--- ds-02-arrays/src/main/java/Assignment2_NumOfPeopleInTheBus.java
+++ ds-02-arrays/src/main/java/Assignment2_NumOfPeopleInTheBus.java
@@ -17,3 +17,19 @@ public class Assignment2_NumOfPeopleInTheBus {
         return numOfPeople;
     }
 }
+/*
+1. There is a bus moving in the city, and it takes and drop some people in each bus stop.
+You are provided with a list (or array) of integer pairs. Elements of each pair represent number of people
+get into bus (The first item) and number of people get off the bus (The second item) in a bus stop.
+
+Your task is to return number of people who are still in the bus after the last bus station (after the last
+array). Even though it is the last bus stop, the bus is not empty and some people are still in the bus, and
+they are probably sleeping there :D
+Take a look on the test cases next page.
+
+Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So
+the return integer can't be negative.
+The second value in the first integer array is 0, since the bus is empty in the first bus stop.
+
+2. What is the time complexity of your solution?
+ */
