c1dbf93db5876b8a17f470de6fd3a173b32bc3ed
nihano <nioztekin@gmail.com>
2023-02-26 21:52:44 +0300

    MyTree insert

---
+++ ds-08-trees/src/main/java/MyTree.java
@@ -0,0 +1,37 @@
+public class MyTree {
+    TNode root;
+
+    public MyTree() {
+    }
+
+    void insert(int value){
+        TNode newNode= new TNode(value);
+        if (root==null){
+            root=newNode;
+            return;
+        }
+        TNode current = root;
+        while (true){
+            if (value<= current.value){
+                if (current.leftChild==null){
+                    //if left is null insert there!!
+                    current.leftChild=newNode;
+                    break;
+                }
+                //if current is not null
+                current=current.leftChild;
+            }
+            //if value is greater than the current value
+            else {
+                if (current.rightChild==null){
+                    //if right is null insert there!!
+                    current.rightChild=newNode;
+                    break;
+                }
+                current=current.rightChild;
+
+            }
+
+        }
+    }
+}
