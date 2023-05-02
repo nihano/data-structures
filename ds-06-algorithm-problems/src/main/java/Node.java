b9bfcd4de9a27ca378625b7473c9665437e0aaa8
nihano <nioztekin@gmail.com>
2023-05-02 23:18:12 +0300

    Node

---
+++ ds-06-algorithm-problems/src/main/java/Node.java
@@ -0,0 +1,8 @@
+public class Node {
+    int id;
+    Node next; //we are referring to another elements address.default value will be null.
+
+    public Node(int id) {
+        this.id = id;
+    }
+}
