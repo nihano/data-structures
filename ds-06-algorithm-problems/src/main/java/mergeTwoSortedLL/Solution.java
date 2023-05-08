1a158b031dce6a507d751b47d066005761eeb654
nihano <nioztekin@gmail.com>
2023-05-08 17:25:26 +0300

    merge two linked list

---
+++ ds-06-algorithm-problems/src/main/java/mergeTwoSortedLL/Solution.java
@@ -0,0 +1,23 @@
+package mergeTwoSortedLL;
+
+public class Solution {
+    public ListNode mergeTwoLists(ListNode curr1, ListNode curr2){
+        //create dummy head node
+        ListNode tempHead= new ListNode();
+        ListNode prev = tempHead;
+        while (curr1!=null && curr2!=null){
+            if (curr1.val<=curr2.val){
+                prev.next=curr1;
+                curr1=curr1.next;
+            }else{
+                prev.next=curr2;
+                curr2=curr2.next;
+            }
+            prev=prev.next;
+        }
+        //now check if any leftover
+        if (curr1==null) prev.next=curr2;
+        else prev.next=curr1;
+        return tempHead.next;
+    }
+}
