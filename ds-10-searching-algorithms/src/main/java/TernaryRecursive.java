c313b688ae0634d80d5da35a833abad9c3cd959a
nihano <nioztekin@gmail.com>
2024-06-22 16:55:41 +0300

    ternary search recursive

---
+++ ds-10-searching-algorithms/src/main/java/TernaryRecursive.java
@@ -0,0 +1,26 @@
+public class TernaryRecursive {
+    public static int ternaryRecursive(int[] arr, int data, int left, int right){
+
+        if (left>right) return -1;
+
+        int partition = (right-left)/3;
+        int middle1 = left+partition;
+        int middle2 = right-partition;
+
+        if (arr[middle1]==data) return middle1;
+        if (arr[middle2]==data) return middle2;
+
+        if (data<arr[middle1]){
+            return ternaryRecursive(arr, data, left, middle1-1);
+        } else if (data>arr[middle2]) {
+            return ternaryRecursive(arr,data,middle2+1, right);
+        }else{
+            return ternaryRecursive(arr, data, middle1+1, middle2-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,5,10,18,21,30};
+        System.out.println(ternaryRecursive(arr, 10,0, arr.length-1));
+    }
+}
