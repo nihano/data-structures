8990392bed1ac825366a209ced17f2392ebbc707
nihano <nioztekin@gmail.com>
2024-06-22 15:56:02 +0300

    commit

---
+++ ds-10-searching-algorithms/src/main/java/LinearSearch.java
@@ -0,0 +1,11 @@
+public class LinearSearch {
+
+    public int linearSearch(int[] arr, int data){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]==data){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
