d0803ff08e331841333ede4fad7e9956e56c08bd
nihano <nioztekin@gmail.com>
2024-06-22 16:02:54 +0300

    binary search iterative

---
+++ ds-10-searching-algorithms/src/main/java/BinarySearchIterative.java
@@ -0,0 +1,27 @@
+public class BinarySearchIterative {
+
+    static int binarySearchIterative(int[] arr, int data){
+        int left = 0;
+        int right = arr.length-1;
+
+        while (left<right){
+            int middle = (right-left)/2;
+            if (arr[middle]==data){
+                return middle;
+            }else if (data<arr[middle]){
+                right=middle-1;
+            }else{
+                left=middle+1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,5,10,18,21,30};
+        System.out.println(binarySearchIterative(arr, 10));
+    }
+
+
+}
