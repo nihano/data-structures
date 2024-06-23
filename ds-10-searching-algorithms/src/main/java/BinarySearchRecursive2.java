7daf5bd2917d2a0e8ce613be3b7e853115041bfe
nihano <nioztekin@gmail.com>
2024-06-23 17:06:38 +0300

    BinarySearchRecursive

--- ds-10-searching-algorithms/src/main/java/BinarySearchRecursive2.java
+++ ds-10-searching-algorithms/src/main/java/BinarySearchRecursive2.java
@@ -16,6 +16,6 @@ public class BinarySearchRecursive2 {
 
     public static void main(String[] args) {
         int[] arr = {3,7,10,11,34,56};
-        System.out.println(binarySearchRecursive(arr, 11));
+        System.out.println(binarySearchRecursive(arr, 56));
     }
 }
