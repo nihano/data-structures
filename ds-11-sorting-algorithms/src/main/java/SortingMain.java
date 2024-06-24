dd691ad49837f905cc49e3881c6bc70198f103e8
nihano <nioztekin@gmail.com>
2024-06-24 12:27:26 +0300

    SortingMain

---
+++ ds-11-sorting-algorithms/src/main/java/SortingMain.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class SortingMain {
+    public static void main(String[] args) {
+        int[] arr = {7,2,4,1,3};
+        System.out.println(Arrays.toString(BubbleSort.bubbleSort(arr)));
+    }
+}
