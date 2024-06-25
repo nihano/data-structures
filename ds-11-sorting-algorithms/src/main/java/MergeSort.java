7594dd8ac19e1e549d1070d6b78cf682cc252879
nihano <nioztekin@gmail.com>
2024-06-25 17:26:33 +0300

    MergeSort

---
+++ ds-11-sorting-algorithms/src/main/java/MergeSort.java
@@ -0,0 +1,40 @@
+public class MergeSort {
+    public static void mergeSort(int[] arr) {
+        if (arr.length < 2) return;
+        int mid = arr.length / 2;
+
+        int[] leftArray = new int[mid];
+        for (int i = 0; i < mid; i++) {
+            leftArray[i]=arr[i];
+        }
+
+        int[] rightArray = new int[arr.length-mid];
+        for (int i = mid; i < arr.length; i++) {
+            rightArray[i-mid]=arr[i];
+        }
+
+        mergeSort(leftArray);
+        mergeSort(rightArray);
+        merge(leftArray,rightArray,arr);
+    }
+
+    public static void merge(int[] left, int[] right, int[] result) {
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                result[k++] = left[i++];
+            } else {
+                result[k++] = right[j++];
+            }
+        }
+
+        while (i < left.length) {
+            result[k++] = left[i++];
+        }
+        while (j < right.length) {
+            result[k++] = right[j++];
+        }
+    }
+
+
+}
